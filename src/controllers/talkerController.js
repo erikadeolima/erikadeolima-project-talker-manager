975f8fb5172d46f2c86005ae1e23d6cbb31ce75d
your_erikadeolima <your_erikadeo.lim@hotmail.com>
2022-08-26 18:51:57 -0300

    feat: requisito numero 1 concluido, com melhoria

--- src/controllers/talkerController.js
+++ src/controllers/talkerController.js
@@ -4,19 +4,18 @@ const readerTalkerFile = require('../readerTalkerFile');
 const talkerRouter = express.Router();
 
 talkerRouter.get('/', async (req, res) => {
-  /* try {
+  try {
     const talker = await readerTalkerFile.readFile();
     return res.status(200).json(talker);
   } catch (err) {
     console.log(err);
     return res.status(200).json([]);
-  } */
-  /*  */
-  const talker = await readerTalkerFile.readFile();
+  }
+  /* const talker = await readerTalkerFile.readFile();
   if (!talker) {
     return res.status(200).json([]);
   } 
-  return res.status(200).json(talker);
+  return res.status(200).json(talker); */
 });
 
 module.exports = talkerRouter;
