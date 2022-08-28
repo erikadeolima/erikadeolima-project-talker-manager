99a7bcc6f3078f3548ed9d038dcb15fd6564903a
your_erikadeolima <your_erikadeo.lim@hotmail.com>
2022-08-28 17:03:22 -0300

    feat: requisitos 3 e 4

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,15 +1,13 @@
 const express = require('express');
-// const fs = require('fs').promises;
-const validatePassword = require('../middlewares/validatePassword.js');
 const tokenGenerate = require('../untils/generateToken');
 const validateEmail = require('../middlewares/validateEmail');
+const validatePassword = require('../middlewares/validatePassword');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', validateEmail, validatePassword, async (req, res) => {
+loginRouter.post('/', validateEmail, validatePassword, (req, res) => {
   const { email, password } = req.body;
-  const token = await tokenGenerate();
-  // fs.writeFile('../userAuth.json', JSON.stringify([email, password, token]));
+  const token = tokenGenerate();
   res.status(200).json({ email, password, token });
 });
 
